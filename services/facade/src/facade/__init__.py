2f795a88e521d885197867a69e5f9072bbbc9ddb
timofey282228 <tymopod-ipt25@lll.kpi.ua>
2026-04-11 14:42:50 +0200

    retries for get

--- services/facade/src/facade/__init__.py
+++ services/facade/src/facade/__init__.py
@@ -1,12 +1,13 @@
 import asyncio
 import datetime
+import functools
 import logging
 import random
 import time
 from abc import ABCMeta
 from collections.abc import Coroutine, Mapping
 from contextlib import asynccontextmanager
-from typing import Annotated, Iterable
+from typing import Annotated, Awaitable, Callable, Iterable
 from uuid import UUID, uuid7
 
 import httpx
@@ -14,9 +15,8 @@ import pydantic
 from fastapi import Body, FastAPI, Query, Request
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-LOGGING_SERVICE_URL = httpx.URL("http://logging")
 COUNTER_SERVICE_ENDPOINT = httpx.URL("http://counter")
-MAX_TIMEOUT_LOGGING = 32
+MAX_ETO = 32
 
 # MARK: models
 
@@ -123,48 +123,69 @@ class LoggingService(_Timed):
     ):
         super().__init__()
         self.http_client = http_client
-        self.valid_logging_instances = tuple(logging_instances)
+        self.logging_instances = tuple(logging_instances)
 
     def random_instance(self):
         return random.choice(self.logging_instances)
+
+    @staticmethod
+    async def _with_eto_retry_on[O](
+        exception_class: type[Exception],
+        operation_builder: Callable[[], Awaitable[O]],
+        on_error: Callable[[Exception], None],
+    ) -> O:
+        eto = 1 / 8
+        while True:
+            try:
+                return await operation_builder()
+            except exception_class as e:
+                on_error(e)
+
+            await asyncio.sleep(eto)
+            if eto < MAX_ETO:
+                eto *= 2
+
+    async def _post_add_transaction(
+        self,
+        transaction_id: UUID,
+        logged_transaction_data: TransactionData,
+    ):
+        url = httpx.URL(
+            scheme="http",
+            host=self.random_instance(),
+            path=f"/transactions/{transaction_id}",
+        )
+        (
+            await self.timed(
+                self.http_client.post(
+                    url,
+                    headers={"content-type": "application/json"},
+                    content=logged_transaction_data.model_dump_json(),
+                )
+            )
+        ).raise_for_status()
 
     async def add_transaction(
         self,
         transaction_id: UUID,
         logged_transaction_data: TransactionData,
     ):
-        eto = 1 / 8
-        while True:
-            url = httpx.URL(
-                scheme="http",
-                host=self.random_instance(),
-                path=f"/transactions/{transaction_id}",
-            )
+        await self._with_eto_retry_on(
+            httpx.TransportError,
+            operation_builder=functools.partial(
+                self._post_add_transaction,
+                transaction_id=transaction_id,
+                logged_transaction_data=logged_transaction_data,
+            ),
+            on_error=lambda e: logging.exception(
+                "Add transaction %s/%s: exception",
+                logged_transaction_data.user_id,
+                transaction_id,
+                exc_info=e,
+            ),
+        )
 
-            try:
-                (
-                    await self.timed(
-                        self.http_client.post(
-                            url,
-                            headers={"content-type": "application/json"},
-                            content=logged_transaction_data.model_dump_json(),
-                        )
-                    )
-                ).raise_for_status()
-                break
-            except httpx.TransportError as e:
-                logging.exception(
-                    "%s/%s: exception",
-                    logged_transaction_data.user_id,
-                    transaction_id,
-                    exc_info=e,
-                )
-
-            asyncio.sleep(eto)
-            if eto < MAX_TIMEOUT_LOGGING:
-                eto *= 2
-
-    async def get_transactions(self, user_id: UserId) -> TransactionList:
+    async def _get_get_transactions(self, user_id: UserId) -> TransactionList:
         transactions_response = (
             await self.http_client.get(
                 httpx.URL(
@@ -177,6 +198,20 @@ class LoggingService(_Timed):
 
         transactions = TransactionList.model_validate_json(transactions_response.read())
         return transactions
+
+    async def get_transactions(self, user_id: UserId) -> TransactionList:
+        return await self._with_eto_retry_on(
+            httpx.TransportError,
+            operation_builder=functools.partial(
+                self._get_get_transactions,
+                user_id=user_id,
+            ),
+            on_error=lambda e: logging.exception(
+                "Get transactions for %s: exception",
+                user_id,
+                exc_info=e,
+            ),
+        )
 
 
 class CounterService(_Timed):
